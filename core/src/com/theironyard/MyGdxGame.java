b6d7fbe3462afb3becf681d78cf5a781c3e06135
Steven Burris <stevenburris.jr@gmail.com>
2016-09-30 19:09:42 -0400

    Finished animations;

--- core/src/com/theironyard/MyGdxGame.java
+++ core/src/com/theironyard/MyGdxGame.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -21,7 +22,7 @@ public class MyGdxGame extends ApplicationAdapter {
     static final int DRAW_HEIGHT = HEIGHT *3;
     static final int GRAVITY = 0;
 
-    TextureRegion down, up, right, left, stand;
+    TextureRegion down, up, right, left;
 
 
     String direction = "faceDown";
